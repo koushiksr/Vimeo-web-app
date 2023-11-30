530927663424a4170f14d2c58e90bd05fd9dc18e
koushiksr <koushiksr1999@gmail.com>
2023-12-01 03:14:52 +0530

    ejs commit removed react code  only backend code

--- routes/vimeoRoute.js
+++ routes/vimeoRoute.js
@@ -6,8 +6,7 @@ router.post('/', async (req, res) => {
   try {
     const {videoID,description,date} = req.body
     const newData = await  vimeo.create({videoID,description,date});
-    console.log(newData);
-    newData?res.status(201).send({newData}):res.status(404).send({msg:'error storing video data in our DB'})
+    (newData||newData!=null)?res.render('home'):res.status(404).send({msg:'error storing video data in our DB'})
   } catch (error) {
     res.status(500).send({ error: 'error storing video data in our DB'}); 
   }
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const vimeoRoute=require('./routes/vimeoRoute')
 require('./models/db')
 require('dotenv').config();
-
 const app = express()
-app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.locals.env = process.env;
+    next();
+  });
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
 
-app.use('/upload', require('./routes/vimeoRoute'))
+app.use(bodyParser.json())
+app.get('/', (req, res) => {res.render('home')});
+app.use('/upload',vimeoRoute)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`App running on port ${PORT}`)  )
