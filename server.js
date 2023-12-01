e6965d6cd48f4005100a1cb109a15df461d96c1b
koushiksr <koushiksr1999@gmail.com>
2023-12-01 12:15:43 +0530

    final commit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
     next();
   });
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.use(express.static(__dirname + '/views'));
 
 app.use(bodyParser.json())
